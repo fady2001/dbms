ae9e4fc044288d8b781eb2d0cd8d7fa00f577de3
Fady Adel <75928317+fady2001@users.noreply.github.com>
2024-11-10 11:43:48 +0200

    run button

--- src/components/CustomAppBar.jsx
+++ src/components/CustomAppBar.jsx
@@ -1,21 +1,32 @@
-import * as React from 'react';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import Typography from '@mui/material/Typography';
-import {AppBar} from './styles'; 
-import ModeSwitch from './ModeSwitch';
+import * as React from "react";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Typography from "@mui/material/Typography";
+import { AppBar } from "./styles";
+import ModeSwitch from "./ModeSwitch";
+import { Box } from "@mui/material";
+import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
 
 export default function CustomAppBar({ open, handleDrawerOpen }) {
   return (
-    <AppBar position="fixed" open={open} sx={{display:'flex' , flexDirection:"row" ,justifyContent:"space-between",alignItems:'center'}}>
+    <AppBar
+      position="fixed"
+      open={open}
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
       <Toolbar>
         <IconButton
           color="inherit"
           aria-label="open drawer"
           onClick={handleDrawerOpen}
           edge="start"
-          sx={{ marginRight: 5, ...(open && { display: 'none' }) }}
+          sx={{ marginRight: 5, ...(open && { display: "none" }) }}
         >
           <MenuIcon />
         </IconButton>
@@ -23,7 +34,18 @@ export default function CustomAppBar({ open, handleDrawerOpen }) {
           Mini variant drawer
         </Typography>
       </Toolbar>
-      <ModeSwitch />
+      <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          edge="start"
+          sx={{ marginRight: 5, ...(open && { display: "none" }) }}
+        >
+          <PlayCircleFilledIcon color="success" fontSize="large" />
+        </IconButton>
+        <ModeSwitch />
+      </Box>
     </AppBar>
   );
 }
