23bd3080a19572192a219209e4c3dd108022b94c
Fady Adel <75928317+fady2001@users.noreply.github.com>
2024-11-10 13:42:12 +0200

    .

--- src/components/DrawerListItems.jsx
+++ src/components/DrawerListItems.jsx
@@ -4,11 +4,9 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import StorageIcon from '@mui/icons-material/Storage';
 
 const items1 = ['Inbox', 'Starred', 'Send email', 'Drafts'];
-const items2 = ['All mail', 'Trash', 'Spam'];
 
 export default function DrawerListItems({ open }) {
   return (
@@ -18,19 +16,7 @@ export default function DrawerListItems({ open }) {
           <ListItem key={text} disablePadding sx={{ display: 'block' }}>
             <ListItemButton sx={{ minHeight: 48, px: 2.5, justifyContent: open ? 'initial' : 'center' }}>
               <ListItemIcon sx={{ minWidth: 0, mr: open ? 3 : 'auto', justifyContent: 'center' }}>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <List>
-        {items2.map((text, index) => (
-          <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-            <ListItemButton sx={{ minHeight: 48, px: 2.5, justifyContent: open ? 'initial' : 'center' }}>
-              <ListItemIcon sx={{ minWidth: 0, mr: open ? 3 : 'auto', justifyContent: 'center' }}>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                <StorageIcon />
               </ListItemIcon>
               <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
             </ListItemButton>
